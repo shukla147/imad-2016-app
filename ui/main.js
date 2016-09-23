7a6237d967e0bf510854b897e25496aff693f95d
shukla147 <shukla147@users.noreply.github.com>
2016-09-23 21:03:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //move the image
 
-var img = document.getElementById('img');
+var img = document.getElementById('mad1');
 
 img.onclick = function(){
     img.style.marginleft = "100px"
