cc7b2fd764b0f69bc35e4af5be348a18dd34fea9
shukla147 <shukla147@users.noreply.github.com>
2016-10-30 11:22:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,42 +1,9 @@
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 var counter = 0;
-
 button.onclick = function(){
-    
-    counter =counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
 
-/*button.onclick = function(){
-    
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE)
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://shukla147.imad.hasura-app.io/counter ',true);
-    request.send(null);
+counter = counter +1;
+var span = documnet.getElementById('count');
+span.innerHTML = counter.toString();
     
 };
-
-
-
-// sunbmit
-
-var nameInput = document.getElementById('name');
-var submit1 =document.getElementById('submit');
-submit1.onclick = functon(){
-    var name1 = ['name1','name2','name3'];
-    var list = '';
-    for (var i=0; i<name1.length;i++){
-        list += '<li>' + name1[i] + '</li>';
-    }
-    var ul =document.getElementById('namelist');
-    ul.innerHTML = list;
-}*/
