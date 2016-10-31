f6a31fdf8616348c9209757b6854173a8c1bce68
shukla147 <shukla147@users.noreply.github.com>
2016-10-31 11:48:08 +0530

    Create main.js

---
+++ ui/main.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+	var button = document.getElementById('counter');
+
+  var counter = 0;
+
+  button.onclick = function(){
+
+	  counter = counter + 1;
+
+  	var span = document.getElementById('count');
+	  span.innerHTML = counter.toString();
+  };
+};
