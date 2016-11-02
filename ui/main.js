b6760f59168f1818328733a4aa24219d10bce24e
shukla147 <shukla147@users.noreply.github.com>
2016-11-02 20:25:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,8 @@ var submit = getElementById("submit_btn");
 submit.onclick = function(){
     var names = ['name1','name2','name3'];
     var list = '';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' name[i] + '</li>';
+    for (var i = 0; i< names.length; i++){
+        list += '<li>' + names[i] + '</li>'
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
