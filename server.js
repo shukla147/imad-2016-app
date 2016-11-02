1a642f9bd898312d484ae89dd638ffd48e400f9a
shukla147 <shukla147@users.noreply.github.com>
2016-11-02 21:00:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.get('/', function (req, res) {
 
 var names = [];
 app.get('/submit-name',function(req,res){
-        var name = req.query.name;
-        names.push(name);
-        res.send(JSON.stringify(names));
+   
+   var name = req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
