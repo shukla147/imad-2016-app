a3029eac80e479fee780af9555bae76e39a5e110
shukla147 <shukla147@users.noreply.github.com>
2016-11-02 21:13:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,6 +32,33 @@ submit.onclick = function(){
     ul.innerHTML = list;
 };
 
+// another part
+
+
+var nameInput = document.getElementById('name');
+var name= nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    
+var request = new XMLHttpRequest();
+ request.onreadystatechange = function(){
+          if(request.readyState ===XMLHttpRequest.DONE){
+             if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = [];
+                for (var i =0 ; i<names.length;i++ ){
+                    list += '<li>' + names[i]+ '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+             } 
+          }  
+};
+request.open('GET','http://shukla147.cloud.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
+}
+
 
 
 
