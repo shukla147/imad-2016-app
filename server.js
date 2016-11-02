02b864c08374c4e18a0f8e57cfc20e14b942ba16
shukla147 <shukla147@users.noreply.github.com>
2016-11-02 20:36:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/counter', function(req,res){
     res.send(counter.toString());
 });
 
+var names = [];
 app.get('/submit-name:name', function(req,res){
     var name= req.params.name;
     name.push(name);
