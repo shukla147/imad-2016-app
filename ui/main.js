5acbccc3e214ede6fa5d6b2480148253eb144e4a
shukla147 <shukla147@users.noreply.github.com>
2016-11-10 00:01:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ window.onload = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           if(request.status === 200){
               var counter = request.responseText;
-              var span = document.getElementById('span');
+              var span = document.getElementById('count');
               span.innerHTML = counter.toString();
           }
       }  
