39772eab466e0ef3bcdab365b25dbf7036234224
shukla147 <shukla147@users.noreply.github.com>
2016-11-09 21:55:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,21 @@
 window.onload = function(){
-	var button = document.getElementById('counter');
+var button = document.getElementById('counter');
 
-  button.onclick = function(){
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
-          if(request.readyState ===XMLHttpRequest.DONE){
-             if(request.status === 200){
-                 var counter = request.responseText;
-                 	var span = document.getElementById('count');
-	  span.innerHTML = counter.toString();
+button.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+          if(request.status === 200){
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+	         span.innerHTML = counter.toString();
              } 
           }  
         };
         request.open('GET','http://shukla147.imad.hasura-app.io/counter',true);
         request.send(null);
   };
- };
+};
 
 
 // submit name code
@@ -42,7 +42,8 @@ submit.onclick = function(){
     var name = nameInput.value;
     request.open('GET','http://shukla147.imad.hasura-app.io/submit-name?name='+name ,true);
     request.send(null);
-};};
+};
+};
 
 
 
