068a95b321ac4d95142bad719c4e59e0c32bf1eb
shukla147 <shukla147@users.noreply.github.com>
2016-11-09 21:47:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+
+app.get('/counter', function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 
 var names = [];
 app.get('/submit-name',function(req,res){
@@ -33,12 +39,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter = 0;
 
-app.get('/counter', function(req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
 
 
 
