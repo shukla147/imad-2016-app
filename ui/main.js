878eab9371501ce72a6ff8d9110c0f65d744f223
shukla147 <shukla147@users.noreply.github.com>
2016-11-09 23:46:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,18 @@
 window.onload = function(){
-var button = document.getElementById("submit_btn");
-button.onclick = function(){
+  var counter = document.getElementById('counter');
+  counter.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-             if(request.status === 200){
-             var counter = request.responseText;
-             var span = document.getElementById('count');
-	         span.innerHTML = counter.toString();
-             } 
-          }  
-        };
-        request.open('GET','http://shukla147.imad.hasura-app.io/counter' ,true);
-        request.send(null);
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('span');
+              span.innerHTML = counter.toString();
+          }
+      }  
+    };
+    request.open('GET','http://shukla147.imad.hasura-app.io/counter',true);
+    request.send(null);
   };
 };
 
