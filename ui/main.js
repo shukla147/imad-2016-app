b363742df6114a804961d309aafeab2ddea31fbd
shukla147 <shukla147@users.noreply.github.com>
2016-11-10 13:44:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,7 +47,7 @@ submit.onclick = function(){
 function get_todos() {
     var todos = new Array();
     var todos_str = localStorage.getItem('todo');
-    if (todos_str !== null) {
+    if (todos_str === null) {
         todos = JSON.parse(todos_str); 
     }
     return todos;
