337923a2bd0e131e3461abdbc9ca46f2e2c38074
shukla147 <shukla147@users.noreply.github.com>
2016-11-10 13:52:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,13 +55,10 @@ function get_todos() {
 
 function add() {
     var task = document.getElementById('task').value;
-
     var todos = get_todos();
     todos.push(task);
     localStorage.setItem('todo', JSON.stringify(todos));
-
     show();
-
     return false;
 }
 
