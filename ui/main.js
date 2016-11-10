5369e09ac16f03ccd1f8811f2447c8235ba74676
shukla147 <shukla147@users.noreply.github.com>
2016-11-10 13:10:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,14 +44,14 @@ submit.onclick = function(){
 
 // TODO app 
 
-/*function get_todos() {
+function get_todos() {
     var todos = new Array;
     var todos_str = localStorage.getItem('todo');
     if (todos_str !== null) {
         todos = JSON.parse(todos_str);
     }
     return todos;
-}*/
+}
 
 function add() {
     var task = document.getElementById('task').value;
