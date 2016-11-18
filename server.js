50936a33eada1621a32e6fac9eb7dbdca7eb69a8
shukla147 <shukla147@users.noreply.github.com>
2016-11-18 09:48:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,13 @@ var blogs = {
                      </div>
                </div>
            </div>
-    <script type="text/javascript" src="/ui/main.js"></script>
+    <script> var button=document.getElementById('likebutton');
+var likebutton = 0;
+button.onclick= function() {
+      likebutton=likebutton + 1;
+      var span=document.getElementById('likes');
+      span.innerHTML = button.toString();
+  };</script>
        </body>
     </html>
 
