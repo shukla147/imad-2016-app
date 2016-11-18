21ebd599fbc38c7bdfec9d26d4fee601b28f7d39
shukla147 <shukla147@users.noreply.github.com>
2016-11-18 11:30:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-var button=document.getElementById('likebutton');
-var likebutton = 0;
+var button=document.getElementById('counter');
+var counter = 0;
 button.onclick= function() {
-      likebutton=likebutton + 1;
-      var span=document.getElementById('likes');
-      span.innerHTML = button.toString();
+      counter += 1;
+      var span=document.getElementById('count');
+      span.innerHTML = counter.toString();
   };
