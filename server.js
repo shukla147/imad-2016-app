00c8a1a364168ffac560acde5f0d617b36f003c3
shukla147 <shukla147@users.noreply.github.com>
2016-11-22 08:59:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,6 +148,12 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+app.get('/favicon.ico', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
