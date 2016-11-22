4e621398bde14f4ec39f5ae013136ce38127fece
shukla147 <shukla147@users.noreply.github.com>
2016-11-22 08:42:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var blogs = {
  }
 };
  function createTemplate(data){
-     var title= data.title;
+     //var title= data.title;
      var heading= data.heading;
      var date= data.date;
      var content= data.content;
