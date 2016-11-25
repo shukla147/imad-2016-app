1523aede079c69d803ba524078c639af966d2782
shukla147 <shukla147@users.noreply.github.com>
2016-11-25 12:37:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -267,7 +267,7 @@ app.get('/blogs/:blogNo', function(req, res) {
        if(err){
            res.status(500).send(err.toString());
        } else{
-           if(result.length.rows === 0){
+           if(result.rows.length === 0){
                res.status(404).send('Article Not Found');
            }else{
                 var articleData = result.rows[0];
