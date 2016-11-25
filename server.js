13c0bba88cf31be36c1c753d421228e2803757c6
shukla147 <shukla147@users.noreply.github.com>
2016-11-25 12:39:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ function createTemplate (data){
                </div>
                <div class="container1" >
                      <div>
-                         ${date}.toDateString()
+                         ${date.toDateString()}
                      </div>
                      <div class="blogs">
                          ${content}
