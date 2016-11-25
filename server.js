8a4ccb90fa4c0e5333a313fefa64a486221aba74
shukla147 <shukla147@users.noreply.github.com>
2016-11-25 11:26:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,10 @@ var title= data.title;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/ui/about.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
+});
 var pool= new Pool(config);
 app.get('/test-db', function(req, res) {
     pool.query('SELECT * FROM test', function(err, result) {
